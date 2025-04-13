c36603d6f55df0230d5c54e82b6288f3f3146667
BPernes <bernardopvsouza@gmail.com>
2025-04-13 20:17:01 -0300

    feat: import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@': path.resolve('./src'),
+			'@components': path.resolve('./src/components/'),
+		},
+	},
 	plugins: [
 		react(),
 		tailwindcss()
