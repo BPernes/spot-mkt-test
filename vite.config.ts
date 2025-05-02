0bea58468c9fe81b7276b113430d7e412a9eee49
BPernes <bernardopvsouza@gmail.com>
2025-05-02 17:17:03 -0300

    fix: import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve('./src'),
-      '@components': path.resolve('./src/components/'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  plugins: [react(), tailwindcss()],
 });
